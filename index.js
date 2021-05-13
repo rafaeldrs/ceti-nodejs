65b1504a067d31d063ef6658e76b3c5a7968107f
administrador <Rafael>
2021-05-13 16:16:12 +0200

    Todo

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer(requestListener);
 server.listen(port);
+
+
+/*
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://administrador:I5m4GR0rJdhBztMB@cluster0.cfb0j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+*/
