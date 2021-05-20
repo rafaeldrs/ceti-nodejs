e0cb47f701911da16c514f69c34a76059415570d
rafael <rafael@equipo-6.technicolor.net>
2021-05-20 10:07:16 +0200

    Plantilla de css

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 // App
 const app = express();
+
+app.use(express.static('views'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/' + 'index.html');
 });
