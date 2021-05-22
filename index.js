407e0cc1eed3e056493146151a87d49065c9f55a
rafael <rafael@equipo-6.home>
2021-05-22 16:51:49 +0200

    Pre-cambios

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 // Constants
 const PORT = process.env.PORT || 5000;
@@ -8,14 +10,42 @@ const PORT = process.env.PORT || 5000;
 // App
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+  next();
+});
+
+// LA CSP es compleja de configurar
+app.use(helmet.contentSecurityPolicy({
+  useDefaults: true,
+  directives: {
+    defaultSrc: ["'self'", "https://fonts.googleapis.com"],
+    "script-src": ["'self'", "https://code.jquery.com"],
+    "style-src": null,
+  }
+}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(cors())
+
 app.use(express.static('views'));
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/' + 'index.html');
 });
 
-app.listen(PORT);
-console.log(`Running on ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`En el puerto: ${PORT}`);
+});
 
 
 
